0cae14663c4506d59bff62e03698472e2f0c9356
kamalabot <kamaljp@gmail.com>
2024-01-16 07:24:44 +0530

    practical-pytorch is committed

---
+++ char_classifier_rnn.py
@@ -0,0 +1,312 @@
+import random
+import math
+import time
+from io import open
+import glob
+import os
+import unicodedata
+import string
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+def findFiles(path):
+    return glob.glob(path)
+
+
+file_path = 'D:\\gitFolders\\pytorch_hardway\\data\\names_data\\names\\*.txt'
+# print(findFiles(file_path))
+
+all_letters = string.ascii_letters + ".,;'"
+n_letter = len(all_letters)
+
+
+# To turn a Unicode string to plain ASCII
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+
+# print(unicodeToAscii('Ślusàrski'))  # Slusarski
+
+
+category_lines = {}
+all_categs = []
+
+
+# Read a file & split into lines
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(c) for c in lines]
+
+
+for filename in findFiles(file_path):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categs.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+
+n_categs = len(all_categs)
+
+# print(all_categs, n_categs)
+# print(category_lines['Greek'][:3])
+
+# Starting to convert the words into tensors
+# To represent a single letter, we use a “one-hot vector” of size <1 x n_letters>. A one-hot vector is filled with 0s except for a 1 at index of the current letter, e.g. "b" = <0 1 0 0 0 ...>.
+
+
+def letterToIndex(letter):
+    """Returns the index of the letter inside 
+    all_letters array"""
+    return all_letters.find(letter)
+
+
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letter)
+    # in the first element, locate the 
+    # letter's index and make it one
+    # leave the rest as 0
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letter)
+    # each line will contain characters, and each char 
+    # has to be tensor with 1r X n_letter c. 
+    for li, letter in enumerate(line):
+        # Each char is one-hot encoded &
+        # entire char is 1R X n_letter C tensor 
+        # Char tensor is batched inside tensor
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+
+# print(letterToIndex('T'))  # 34
+# print(letterToTensor('T'))
+""" LetterToTensor Output
+[[0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0.]]
+"""
+# print(lineToTensor('Theodore'))
+# print(n_letter)
+# print(lineToTensor('Theodore').size())  # Size([8, 1, 56])
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(in_features=input_size + hidden_size,
+                             out_features=hidden_size)
+        # hidden_size is for the tensor that needs to be predicted 
+        # based on the input_size tensor that is provided by the 
+        # current letter 
+        self.h2o = nn.Linear(in_features=hidden_size,
+                             out_features=output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.h2o(hidden)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        # initing the hidden as zeros to begin with
+        return torch.zeros(1, self.hidden_size)
+
+
+# initing the RNN
+n_hidden = 128
+rnn = RNN(n_letter, n_hidden, n_categs)
+
+input = lineToTensor('Albert')  # Size([5, 1, 56])
+hidden = torch.zeros(1, n_hidden)
+
+# get the next letter and the next hidden
+output, next_hidden = rnn(input[0], hidden)
+# print(output)
+# print(next_hidden)
+# How is the next_hidden helping to find the categories?
+
+
+def categFromOutput(output):
+    """Returns the category and its index"""
+    top_n, top_i = output.topk(1)  # Values=tensor([[1.8665]]), indices=tensor([[5]])
+    category_i = top_i[0].item()  # 1.8665186378
+    return all_categs[category_i], category_i
+
+
+# print(categFromOutput(output))   # ('Polish', 12)
+
+# accessing the training samples with ease
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+
+def randomTrainingExample():
+    categ = randomChoice(all_categs)  # 0 to 18
+    name = randomChoice(category_lines[categ])  # one of names from above categ 
+    categ_tensor = torch.tensor([all_categs.index(categ)],
+                                dtype=torch.long)
+    name_tensor = lineToTensor(name)
+    return categ, name, categ_tensor, name_tensor
+
+
+for i in range(10):
+    category, name, category_tensor, name_tensor = randomTrainingExample()
+    print('Category= ', category, '/ line: ', name)
+
+# setting up the criterion to measure the loss
+criterion = nn.NLLLoss()
+
+""" Training Loop:
+- Create input(name) and target(category) tensors
+- Create a zeroed initial hidden state
+- Read each letter in and
+    + Keep hidden state for next letter
+- Compare final output to target
+- Back-propagate
+- Return the output and loss
+"""
+
+# set a appropriate Learning Rate. Too High will explode and too low will 
+# not allow the model to learn
+learning_rate = 0.005
+
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()  # get a hidden tensor
+
+    rnn.zero_grad()  # zero the gradients
+    # enumerate the tensors on the line_tensor
+    for i in range(line_tensor.size()[0]):
+        # predict the category & hidden tensor
+        output, hidden = rnn(line_tensor[i],
+                             hidden)
+    # Compare the prediction with the target category
+    loss = criterion(output, category_tensor)
+    # Backpropagate
+    loss.backward()
+    # Update the paramters of the model
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+
+    return output, loss.item() # return pred and loss
+
+
+n_iters = 1000
+print_every = 500
+plot_every = 1000
+
+# track loss
+current_loss = 0
+all_losses = []
+
+
+def timeSince(since):
+    now = time.time()  # current time
+    s = now - since  # difference
+    m = math.floor(s / 60)  # calculate mins
+    s -= m * 60  # remaining seconds
+    return '%dm %ds' % (m, s)  # return string
+
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, name, category_tensor, name_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, name_tensor)
+    current_loss += loss
+
+    # print iter number, loss, name n guess
+    if iter % print_every == 0:
+        guess, guess_i = categFromOutput(output)
+        correct = '✔' if guess == category else '❌ (%s)' % category
+        print(f"""{iter}, {iter/n_iters * 100},
+              {timeSince(start)}, {loss}, {name},
+              {guess}, {correct}""")
+
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every) 
+        current_loss = 0
+
+plt.figure()
+plt.plot(all_losses)
+
+# Starting Eval
+# We will create a confusion matrix, indicating for every actual language (rows) which language the network guesses (columns)
+
+# confusion matrix of 18 X 18 size
+
+confusion = torch.zeros(n_categs, n_categs)
+n_confuson = 1000
+
+
+def evaluate(name_tensor):
+    hidden = rnn.initHidden()
+    for i in range(name_tensor.size()[0]):
+        output, hidden = rnn(name_tensor[i], hidden)
+    return output
+    # there is no back propagation
+
+
+for i in range(n_confuson):
+    category, name, category_tensor, name_tensor = randomTrainingExample()
+    # Generate random samples
+    output = evaluate(name_tensor)  # get the output from model
+    guess, guess_i = categFromOutput(output)  # get category from output
+    category_i = all_categs.index(category)  # get category index
+    confusion[category_i][guess_i] += 1  # update confusion matrix value
+
+# norm the confusion matrix
+for i in range(n_categs):
+    confusion[i] = confusion[i] / confusion[i].sum()
+
+# create plot setup
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+# setup axes
+ax.set_xticklabels([''] + all_categs, rotation=90)
+ax.set_yticklabels([''] + all_categs)
+
+# force label in each tick
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+# show the plot
+plt.show()
+
+
+def predict(input_line, n_pred=3):
+    print(f"> {input_line}")
+    with torch.no_grad():
+        output = evaluate(lineToTensor(input_line))
+
+        topv, topi = output.topk(n_pred, 1, True)
+        predictions = []
+
+        for i in range(n_pred):
+            value = topv[0][i].item()
+            categ_index = topi[0][i].item()
+            print(f"{value:.2f} {all_categs[categ_index]}")
+            predictions.append(value, all_categs[categ_index])
+
+
+predict('Dovesky')
+predict('Lucas')
+predict('Sundaram')
+
--- diving_into_optimisers.py
+++ diving_into_optimisers.py
@@ -1,5 +1,5 @@
 # Script that dives into the details of Optimisers
-
+import numpy as np
 import torch
 from torch import nn
 from torch import optim
@@ -60,7 +60,21 @@ print(model.layer2.weight[0][:10])
 
 # starting optimiser work
 
+def SGD(data, batch_size, lr):
+    N = len(data)
+    np.random.shuffle(data)
+    mini_batches = np.array([data[i:i+batch_size]
+    for i in range(0, N, batch_size)]):
+        for X,y in mini_batches:
+            backprop(X, y, lr) 
+
 sgd_optimiser = optim.SGD(model.parameters(), lr=0.001)
+"""
+SGD is not used much these days as it get stuck in local minima.
+Adagrad, Adadelta, RMSprop, and ADAM generally handle saddle points better.
+SGD with momentum renders some speed to the optimization and also helps escape local minima better.
+Averaged Stochastic Gradient Descent(ASGD) algorithm
+"""
 
 pred = model(some_in)
 
@@ -90,3 +104,94 @@ sgd_optimiser.step()
 # print(model.layer2.weight.grad[0][:10])
 print("Weights after running optimiser 1 times.Change")
 print(model.layer2.weight[0][:10])
+
+
+# Starting the code from Ultimate Optimizers
+
+# Adam is One of the most popular optimizers
+# also known as adaptive Moment Estimation, it combines 
+# the good properties of Adadelta and RMSprop optimizer 
+# into one and hence tends to do better for most of the problems
+adam_optimiser = optim.Adam(model.parameters(), lr=0.001,
+                            betas=(0.9, 0.999),
+                            eps=1e-8, weight_decay=0,
+                            amsgrad=False)
+
+#  practically that AdamW yields better training loss,
+# that means the models generalize much better than
+# models trained with Adam allowing the remake to
+# compete with stochastic gradient descent with momentum.
+adamw_optimiser = optim.AdamW(params=model.parameters(),
+                              lr=0.001, betas=(0.9, 0.999),
+                              eps=1e-8, weight_decay=0.01,
+                              amsgrad=False)
+
+# SparseAdam Implements a lazy version of Adam algorithm which is suitable for sparse tensors.
+# In this variant of adam optimizer, only moments that show up in the gradient get updated,
+# and only those portions of the gradient get applied to the parameters.
+sparse_adam = optim.SparseAdam(params=model.parameters(),
+                               lr=0.001, betas=(0.9, 0.999),
+                               eps=1e-08)
+
+# Ada-Delta Optimiser Code
+# method dynamically adapts over time using only first order information and 
+# has minimal computational overhead beyond vanilla stochastic gradient 
+# descent. The method requires no manual tuning of a learning rate and
+# appears robust to noisy gradient information, different model architecture 
+# choices, various data modalities and selection of hyperparameters.
+def Adadelta(weights, sqrs, deltas, rho, batch_size):
+    eps_stable = 1e-5
+    for weight, sqr, delta in zip(weights, sqrs, deltas):
+        g = weight.grad / batch_size
+        sqr[:] = rho * sqr + (1. - rho) * np.square(g)
+        cur_delta = np.sqrt(delta + eps_stable) / np.sqrt(sqr + eps_stable) * g
+        delta[:] = rho * delta + (1. - rho) * cur_delta * cur_delta
+        # update weight in place.
+        weight[:] -= cur_delta
+
+"""
+rho (float, optional) – coefficient used for computing a running average
+of squared gradients (default: 0.9)
+
+eps (float, optional) – term added to the denominator to improve
+numerical stability (default: 1e-6)
+
+lr (float, optional) – coefficient that scale delta before it
+is applied to the parameters (default: 1.0)
+
+weight_decay (float, optional) – weight decay (L2 penalty) (default: 0)
+"""
+adadel_optimiser = optim.Adadelta(params=model.parameters(),
+                                  lr=1.0, rho=0.9, eps=1e-6,
+                                  weight_decay=0)
+
+"""
+penalizes the learning rate for parameters that are frequently updated, 
+instead, it gives more learning rate to sparse parameters, 
+parameters that are not updated as frequently.
+"""
+def AdaGrad(data, theta, num_iters, weights, eps, lr):
+    grad_sums = np.zeros(theta.shape[0])
+    for t in range(num_iters):
+        grads = compute_gradients(data, weights)
+        grad_sums += grads ** 2
+        grad_updt = grads / (np.sqrt(grad_sums + eps))
+        weights = weights - lr * grad_updt
+    return weights
+
+
+adagrad_optim = optim.Adagrad(params=model.parameters(), lr=0.01,
+                              lr_decay=0, weight_decay=0,
+                              initial_accumulator_value=0,
+                              eps=1e-10)
+
+
+lbgfs_optimiser = optim.LBFGS(model.parameters(),
+                              lr=1, max_iter=20, max_eval=None,
+                              tolerance_grad=1e-07, tolerance_change=1e-09,
+                              history_size=100, line_search_fn=None)
+
+rmsprop = optim.RMSprop(model.parameters(),
+                        lr=0.01, alpha=0.99, eps=1e-08,
+                        weight_decay=0,
+                        momentum=0, centered=False)
