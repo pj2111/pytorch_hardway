6904fdbf689c7d5cb4125481f4c26b50eb453888
kamalabot <kamaljp@gmail.com>
2024-01-16 11:00:26 +0530

    completed char gen tutorial

--- char_classifier_rnn.py
+++ char_classifier_rnn.py
@@ -1,3 +1,7 @@
+# Pytorch team had provided the
+# below code and function in seperate 
+# modules. Then used a jupyter-nb to 
+# execute the functions.
 import random
 import math
 import time
@@ -8,18 +12,22 @@ import unicodedata
 import string
 import torch
 from torch import nn
+from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-
-def findFiles(path):
-    return glob.glob(path)
-
 
 file_path = 'D:\\gitFolders\\pytorch_hardway\\data\\names_data\\names\\*.txt'
 # print(findFiles(file_path))
 
 all_letters = string.ascii_letters + ".,;'"
 n_letter = len(all_letters)
+category_lines = {}
+all_categs = []
+n_categs = len(all_categs)
+
+
+def findFiles(path):
+    return glob.glob(path)
 
 
 # To turn a Unicode string to plain ASCII
@@ -29,13 +37,7 @@ def unicodeToAscii(s):
         if unicodedata.category(c) != 'Mn'
         and c in all_letters
     )
-
-
 # print(unicodeToAscii('Ślusàrski'))  # Slusarski
-
-
-category_lines = {}
-all_categs = []
 
 
 # Read a file & split into lines
@@ -43,15 +45,6 @@ def readLines(filename):
     lines = open(filename, encoding='utf-8').read().strip().split('\n')
     return [unicodeToAscii(c) for c in lines]
 
-
-for filename in findFiles(file_path):
-    category = os.path.splitext(os.path.basename(filename))[0]
-    all_categs.append(category)
-    lines = readLines(filename)
-    category_lines[category] = lines
-
-
-n_categs = len(all_categs)
 
 # print(all_categs, n_categs)
 # print(category_lines['Greek'][:3])
@@ -100,6 +93,9 @@ def lineToTensor(line):
 # print(lineToTensor('Theodore').size())  # Size([8, 1, 56])
 
 
+# This RNN module (mostly copied from the PyTorch for Torch users tutorial)
+# is just 2 linear layers which operate on an input and hidden state, with a
+# LogSoftmax layer after the output
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
@@ -122,21 +118,7 @@ class RNN(nn.Module):
 
     def initHidden(self):
         # initing the hidden as zeros to begin with
-        return torch.zeros(1, self.hidden_size)
-
-
-# initing the RNN
-n_hidden = 128
-rnn = RNN(n_letter, n_hidden, n_categs)
-
-input = lineToTensor('Albert')  # Size([5, 1, 56])
-hidden = torch.zeros(1, n_hidden)
-
-# get the next letter and the next hidden
-output, next_hidden = rnn(input[0], hidden)
-# print(output)
-# print(next_hidden)
-# How is the next_hidden helping to find the categories?
+        return Variable(torch.zeros(1, self.hidden_size))
 
 
 def categFromOutput(output):
@@ -144,12 +126,10 @@ def categFromOutput(output):
     top_n, top_i = output.topk(1)  # Values=tensor([[1.8665]]), indices=tensor([[5]])
     category_i = top_i[0].item()  # 1.8665186378
     return all_categs[category_i], category_i
-
-
 # print(categFromOutput(output))   # ('Polish', 12)
 
-# accessing the training samples with ease
 
+# accessing the training samples with ease
 def randomChoice(l):
     return l[random.randint(0, len(l) - 1)]
 
@@ -161,28 +141,6 @@ def randomTrainingExample():
                                 dtype=torch.long)
     name_tensor = lineToTensor(name)
     return categ, name, categ_tensor, name_tensor
-
-
-for i in range(10):
-    category, name, category_tensor, name_tensor = randomTrainingExample()
-    print('Category= ', category, '/ line: ', name)
-
-# setting up the criterion to measure the loss
-criterion = nn.NLLLoss()
-
-""" Training Loop:
-- Create input(name) and target(category) tensors
-- Create a zeroed initial hidden state
-- Read each letter in and
-    + Keep hidden state for next letter
-- Compare final output to target
-- Back-propagate
-- Return the output and loss
-"""
-
-# set a appropriate Learning Rate. Too High will explode and too low will 
-# not allow the model to learn
-learning_rate = 0.005
 
 
 def train(category_tensor, line_tensor):
@@ -205,15 +163,6 @@ def train(category_tensor, line_tensor):
     return output, loss.item() # return pred and loss
 
 
-n_iters = 1000
-print_every = 500
-plot_every = 1000
-
-# track loss
-current_loss = 0
-all_losses = []
-
-
 def timeSince(since):
     now = time.time()  # current time
     s = now - since  # difference
@@ -222,73 +171,13 @@ def timeSince(since):
     return '%dm %ds' % (m, s)  # return string
 
 
-start = time.time()
-
-for iter in range(1, n_iters + 1):
-    category, name, category_tensor, name_tensor = randomTrainingExample()
-    output, loss = train(category_tensor, name_tensor)
-    current_loss += loss
-
-    # print iter number, loss, name n guess
-    if iter % print_every == 0:
-        guess, guess_i = categFromOutput(output)
-        correct = '✔' if guess == category else '❌ (%s)' % category
-        print(f"""{iter}, {iter/n_iters * 100},
-              {timeSince(start)}, {loss}, {name},
-              {guess}, {correct}""")
-
-    if iter % plot_every == 0:
-        all_losses.append(current_loss / plot_every) 
-        current_loss = 0
-
-plt.figure()
-plt.plot(all_losses)
-
 # Starting Eval
-# We will create a confusion matrix, indicating for every actual language (rows) which language the network guesses (columns)
-
-# confusion matrix of 18 X 18 size
-
-confusion = torch.zeros(n_categs, n_categs)
-n_confuson = 1000
-
-
 def evaluate(name_tensor):
     hidden = rnn.initHidden()
     for i in range(name_tensor.size()[0]):
         output, hidden = rnn(name_tensor[i], hidden)
     return output
     # there is no back propagation
-
-
-for i in range(n_confuson):
-    category, name, category_tensor, name_tensor = randomTrainingExample()
-    # Generate random samples
-    output = evaluate(name_tensor)  # get the output from model
-    guess, guess_i = categFromOutput(output)  # get category from output
-    category_i = all_categs.index(category)  # get category index
-    confusion[category_i][guess_i] += 1  # update confusion matrix value
-
-# norm the confusion matrix
-for i in range(n_categs):
-    confusion[i] = confusion[i] / confusion[i].sum()
-
-# create plot setup
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cax = ax.matshow(confusion.numpy())
-fig.colorbar(cax)
-
-# setup axes
-ax.set_xticklabels([''] + all_categs, rotation=90)
-ax.set_yticklabels([''] + all_categs)
-
-# force label in each tick
-ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-
-# show the plot
-plt.show()
 
 
 def predict(input_line, n_pred=3):
@@ -306,7 +195,112 @@ def predict(input_line, n_pred=3):
             predictions.append(value, all_categs[categ_index])
 
 
-predict('Dovesky')
-predict('Lucas')
-predict('Sundaram')
+if __name__ == '__main__':
+
+    for filename in findFiles(file_path):
+        category = os.path.splitext(os.path.basename(filename))[0]
+        all_categs.append(category)
+        lines = readLines(filename)
+        category_lines[category] = lines
+
+    # initing the RNN
+    n_hidden = 128
+    rnn = RNN(n_letter, n_hidden, n_categs)
+
+    input = lineToTensor('Albert')  # Size([5, 1, 56])
+    hidden = torch.zeros(1, n_hidden)
+
+    # get the next letter and the next hidden
+    output, next_hidden = rnn(input[0], hidden)
+    # print(output)
+    # print(next_hidden)
+    # How is the next_hidden helping to find the categories?
+
+    for i in range(10):
+        category, name, category_tensor, name_tensor = randomTrainingExample()
+        print('Category= ', category, '/ line: ', name)
+
+    # setting up the criterion to measure the loss
+    criterion = nn.NLLLoss()
+
+    """ Training Loop:
+    - Create input(name) and target(category) tensors
+    - Create a zeroed initial hidden state
+    - Read each letter in and
+        + Keep hidden state for next letter
+    - Compare final output to target
+    - Back-propagate
+    - Return the output and loss
+    """
+
+    # set a appropriate Learning Rate. Too High will explode and too low will 
+    # not allow the model to learn
+    learning_rate = 0.005
+
+    n_iters = 1000
+    print_every = 500
+    plot_every = 1000
+
+    # track loss
+    current_loss = 0
+    all_losses = []
+
+    start = time.time()
+
+    for iter in range(1, n_iters + 1):
+        category, name, category_tensor, name_tensor = randomTrainingExample()
+        output, loss = train(category_tensor, name_tensor)
+        current_loss += loss
+
+        # print iter number, loss, name n guess
+        if iter % print_every == 0:
+            guess, guess_i = categFromOutput(output)
+            correct = '✔' if guess == category else '❌ (%s)' % category
+            print(f"""{iter}, {iter/n_iters * 100},
+                {timeSince(start)}, {loss}, {name},
+                {guess}, {correct}""")
+
+        if iter % plot_every == 0:
+            all_losses.append(current_loss / plot_every) 
+            current_loss = 0
+
+    plt.figure()
+    plt.plot(all_losses)
+# We will create a confusion matrix, indicating for every actual language (rows) which language the network guesses (columns)
+    confusion = torch.zeros(n_categs, n_categs)
+    n_confuson = 1000
+
+    # confusion matrix of 18 X 18 size
+    for i in range(n_confuson):
+        category, name, category_tensor, name_tensor = randomTrainingExample()
+        # Generate random samples
+        output = evaluate(name_tensor)  # get the output from model
+        guess, guess_i = categFromOutput(output)  # get category from output
+        category_i = all_categs.index(category)  # get category index
+        confusion[category_i][guess_i] += 1  # update confusion matrix value
+
+    # norm the confusion matrix
+    for i in range(n_categs):
+        confusion[i] = confusion[i] / confusion[i].sum()
+
+    # create plot setup
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(confusion.numpy())
+    fig.colorbar(cax)
+
+    # setup axes
+    ax.set_xticklabels([''] + all_categs, rotation=90)
+    ax.set_yticklabels([''] + all_categs)
+
+    # force label in each tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    # show the plot
+    plt.show()
+    
+    predict('Dovesky')
+    predict('Lucas')
+    predict('Sundaram')
 
